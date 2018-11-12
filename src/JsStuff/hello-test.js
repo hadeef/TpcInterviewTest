ebbb3892100d1458f914501f1909e967cc935cd4
hadeef <h.faiiazee@gmail.com>
2018-11-12 17:09:50 +1300

    Update hello-test.js

--- src/JsStuff/hello-test.js
+++ src/JsStuff/hello-test.js
@@ -2,7 +2,7 @@
 The test is already set up. Just look in hello.js and make the test pass. 
 */
 require('chai').should();
-var hello = require('../ hello);
+var hello = require('../ hello');
                     
 describe('Hello', function () {
   it('should return hello', function () {
