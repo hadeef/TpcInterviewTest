e97460c3ad39d3a46cee319af87c7b4eb4958997
hadeef <h.faiiazee@gmail.com>
2018-11-12 17:09:17 +1300

    Update hello-test.js

--- src/JsStuff/hello-test.js
+++ src/JsStuff/hello-test.js
@@ -2,10 +2,12 @@
 The test is already set up. Just look in hello.js and make the test pass. 
 */
 require('chai').should();
-
+var hello = require('../ hello);
+                    
 describe('Hello', function () {
   it('should return hello', function () {
     // write a test to check if hello.js returns the string 'hello'
-
+      var result=hello.helloFunction();
+      result.should.equal('Hello');
   });
 });
